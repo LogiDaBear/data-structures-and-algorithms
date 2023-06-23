ee486418f936b7e73a26f41f8ee7d3c91a2b3ebe
Logan Reese <lreese2112@gmail.com>
2023-06-23 22:54:47 +0000

    code challenge 10

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,62 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Queue:
-    """
-    Put docstring here
-    """
+
+    # The constructor initializes the queue.
 
     def __init__(self):
-        # initialization here
-        pass
+        # The front pointer points to the first node in the queue.
+        self.front = None
+        # The rear pointer points to the last node in the queue.
+        self.rear = None
 
-    def some_method(self):
-        # method body here
-        pass
+    # The enqueue method adds a new node to the queue.
+
+    def enqueue(self, value):
+        # Create a new node with the given value.
+        new_node = Node(value)
+        # If the queue is empty, set the front and rear pointers to the new node.
+        if self.front is None:
+            self.front = new_node
+            self.rear = new_node
+        # Otherwise, set the next pointer of the rear node to the new node, and then set the rear pointer to the new node.
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    # The dequeue method removes the first node from the queue.
+
+    def dequeue(self):
+        # If the queue is empty, raise an exception.
+        if self.front is None:
+            raise InvalidOperationError("Queue is empty")
+        # Save the value of the first node.
+        value = self.front.value
+        # Set the front pointer to the next node.
+        self.front = self.front.next
+        # If the front pointer is now None, set the rear pointer to None as well.
+        if self.front is None:
+            self.rear = None
+        return value
+
+    # The peek method returns the value of the first node in the queue, without removing it.
+
+    def peek(self):
+        # If the queue is empty, raise an exception.
+        if self.front is None:
+            raise InvalidOperationError("Queue is empty")
+        return self.front.value
+
+    # The is_empty method returns True if the queue is empty, and False otherwise.
+
+    def is_empty(self):
+        return self.front is None
+
+
+class Node:
+
+    # The constructor initializes a node with the given value.
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,56 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Stack:
     """
-    Put docstring here
-    """
-
+   Create a Stack class that has a top property. It creates an empty Stack when instantiated.
+This object should be aware of a default empty value assigned to top when the stack is created.
+The class should contain the following methods:
+ """
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+# push
+# Arguments: value
+# adds a new node with that value to the top of the # stack with an O(1) Time performance.
+    def push(self, value):
+        node = Node(value)
+        if self.top is None:
+            self.top = node
+        else:
+            node.next = self.top
+            self.top = node
+# pop
+# Arguments: none
+# Returns: the value from node from the top of the stack
+# Removes the node from the top of the stack
+# Should raise exception when called on empty stack
+    def pop(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+# peek
+# Arguments: none
+# Returns: Value of the node located at the top of the # stack
+# Should raise exception when called on empty stack
+    def peek(self):
+        if self.top is None:
+            raise InvalidOperationError('Method not allowed on empty collection')
+        return self.top.value
+# is empty
+# Arguments: none
+# Returns: Boolean indicating whether or not the stack # is empty.
+    def is_empty(self):
+        if self.top is None: 
+            return True
+       
+   
+
