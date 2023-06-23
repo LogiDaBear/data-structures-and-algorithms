d287ac7ea8bb4ca41417cf7f6fd6faeb9a0b366b
Logan Reese <lreese2112@gmail.com>
2023-06-23 00:41:28 +0000

    lab fixed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -7,17 +7,17 @@ class LinkedList:
         self.head = None
     def insert(self, value):
         self.head = Node(value, self.head)
-    # def __str__(self):
-    #     if self.head == None:
-    #      return "NULL"
-    #     else:
-    #         current = self.head
-    #         output = ""
-    #         while current:
-    #             output += f"{{ {current.value} }} -> "
-    #             current = current.next
-    #         output += "NULL"
-    #         return output
+    def __str__(self):
+        if self.head == None:
+         return "NULL"
+        else:
+            current = self.head
+            output = ""
+            while current:
+                output += f"{{ {current.value} }} -> "
+                current = current.next
+            output += "NULL"
+            return output
     def includes(self, value):
         current = self.head
         while current:
@@ -84,60 +84,6 @@ class LinkedList:
             return current_2.value
         except Exception as e:
                 raise TargetError
-def __str__(self):
-        result = ""
-        current = self.head
-        while current:
-            result += str(current.value) + " -> "
-            current = current.next
-        result += "None"
-        return result
-def zip_lists(list1, list2):
-    current1 = list1.head
-    current2 = list2.head
-    if not current1 and not current2:
-        return None
-    results_LL = LinkedList()
-    while current1 and current2:
-        results_LL.append(current1.value)
-        results_LL.append(current2.value)
-        current1 = current1.next
-        current2 = current2.next
-    while current1:
-        results_LL.append(current1.value)
-        current1 = current1.next
-    while current2:
-        results_LL.append(current2.value)
-        current2 = current2.next
-    return str(results_LL)
+
 class TargetError(Exception):
         pass
-#     Write the following method for the Linked List class:
-# kth from end
-# argument: a number, k, as a parameter.
-# Return the node’s value that is k places from the tail of the linked list.
-# You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
-"""
-Create a node class with properties for value stored in node and pointer to next node.
-"""
-"""
-Create a linked list class
-- with head property
-- linked list created upon instantiation
-    """
-"""""
-## (1) insert
- - artuments: value
- - returns: nothing
- - adds new node with that value to the head of the list, with an O(1) Time performance
-"""
-"""
-# (2) includes
-# - arguments: value
-# - returns: boolean; indicates whether that value exists as a node's value somewhere in the list
-"""
-"""
-# (3) to string
-# - arguments: none
-# - returns: string representing all values in the Linked List, formatted as
-"""
