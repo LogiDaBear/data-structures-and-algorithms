e21a54de209465ffd6e04536b60f99ea36ddd700
Logan Reese <lreese2112@gmail.com>
2023-06-28 22:29:51 +0000

    done

---
+++ python/code_challenges/stack-queue-brackets/README.md
@@ -0,0 +1,28 @@
+# Code Challenge 13
+
+## Write the following methods for the Linked List class:
+
+Write a function called validate brackets
+Arguments: string
+Return: boolean
+representing whether or not the brackets in the string are balanced
+There are 3 types of brackets:
+
+Round Brackets : ()
+Square Brackets : []
+Curly Brackets : {}
+
+## Whiteboard Process
+- ![CodeChallenge13](cc13.jpg)
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+
+
+
+## Solution
+
+
+## Contributions:
+- Class of 401d22
+- ChatGPT
--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,24 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
+def multi_bracket_validation(str):
+    stack = []
+    opening_brackets = ['(','[','{']
+    closing_brackets = [')',']','}']
+    
+    
+    for char in str:
+        if char in opening_brackets:
+            stack.append(char)
+        elif char in closing_brackets:
+            if not stack:
+                return False
+            
+           
+            top = stack.pop()
+            opening_index = opening_brackets.index(top)
+            closing_index = closing_brackets.index(char)
+            if opening_index != closing_index:
 
-def multi_bracket_validation():
-    pass
+                return False
+
+    return len(stack) == 0
